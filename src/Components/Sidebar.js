c8f9fc49c65df7ee946ccefb70c82c62ccf87573
Abhishek-26498 <dhiman26abhishek@gmail.com>
2022-03-02 22:38:55 -0800

    New Update1

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import '../Components/css/Sidebar.css'
+import { Link } from 'react-router-dom'
 function Sidebar() {
     return (
         <>
             <div className='side_bar_wrapper'>
-                <a href='/' >HOME</a>
-                <a href='/' >EXPLORE</a>
-                <a href='/' >LIBRARY</a>
-                <a href='/' >RECENT</a>
-                <a href='/' >HISTORY</a>
+                <Link to='/' >HOME</Link>
+                <Link to='/' >EXPLORE</Link>
+                <Link to='/' >LIBRARY</Link>
+                <Link to='/' >RECENT</Link>
+                <Link to='/' >HISTORY</Link>
 
-                {/* <p>About  Press  Copyright  Contact us  Creators  Advertise  Developers
+                <div className='bottom_content'>
+                    <p>About  Press  Copyright  Contact us  Creators  Advertise  Developers
 
-                    Terms  Privacy  Policy & Safety
-                    How Pro-Entertainment works
-                    Test new features
+                        Terms  Privacy  Policy & Safety
+                        How Pro-Entertainment works
+                        Test new features
 
-                    © 2022 Google LLC</p> */}
+                        © 2022 Google LLC</p>
+                </div>
 
             </div>
         </>
