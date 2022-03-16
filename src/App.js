8a34abbe0ef4b4566bc613d6eebda9dfc9fc1b99
Abhishek-26498 <dhiman26abhishek@gmail.com>
2022-03-16 11:22:42 -0700

    xf

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from 'react';
 import Landingpage from './Components/Landingpage';
 import MainApp from './Components/MainApp';
 import Signup from './Components/Signup';
-import Signin from './Components/Signin';
+import SignIn from './Components/SignIn';
 import Library from './Components/Library';
 import { Routes,Route } from 'react-router-dom';
 import Explore from './Components/Explore';
 import './App.css'
+// import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
     <>
+    {/* <Navbar/> */}
     <Routes>
       <Route path='/' element={<Landingpage/>}/>;
       <Route path='/mainapp' element={<MainApp/>}/>;
       <Route path='/signup' element={<Signup/>}/>;
-      <Route path='/signin' element={<Signin/>}/>;
+      <Route path='/signin' element={<SignIn/>}/>;
       <Route path='/explore' element={<Explore/>}/>;
       <Route path='/library' element={<Library/>}/>;
     </Routes>
